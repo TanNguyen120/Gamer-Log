7a4617fc8ab7e9c90987b73b47a41e9fac18f4d7
Tan Nguyen <tannguyen1220@gmail.com>
2023-07-11 03:10:39 +0700

    bare bone header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Header from '@/component/header';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900 text-white'>
       <div className=' w-3/4 flex flex-col'>
-        <div className=' text-4xl'>ha ha</div>
+        <Header />
         <div className=' text-5xl'>hi hi</div>
       </div>
     </main>
---
+++ src/component/header/index.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className='flex flex-row'>
+      <div className=' text-3xl font-semibold text-white font-mono'>
+        T-BackLog
+      </div>
+      <div className=' text-slate-200'>games</div>
+      <div>log a game</div>
+    </div>
+  );
+}
