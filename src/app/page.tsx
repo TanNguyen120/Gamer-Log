b4c51a0d2d82d3796eeab83734a4ae4cc7f30ab4
Tan Nguyen <tannguyen1220@gmail.com>
2023-07-11 13:45:57 +0700

    read the env file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Header from '@/component/header';
-import Image from 'next/image';
+import Header from '@/app/header';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900 text-white'>
       <div className=' w-3/4 flex flex-col'>
         <Header />
-        <div className=' text-5xl'>hi hi</div>
+        <div className=' text-5xl'>hi hi EW </div>
+        <div className=' text-5xl'>{process.env.CLIENT_SECRET} AA</div>
       </div>
     </main>
   );
