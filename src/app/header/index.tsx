017f56f7bb35a7f57419d84dc6e1518765a33849
Tan Nguyen <tannguyen1220@gmail.com>
2023-07-14 13:37:33 +0700

    header done

--- src/app/header/index.tsx
+++ src/app/header/index.tsx
@@ -1,12 +1,16 @@
+import { MdOutlineExplore } from 'react-icons/md';
+import SearchComponent from './searchBar';
+
 export default function Header() {
   return (
-    <div className='flex flex-row'>
+    <div className='flex flex-row items-center p-4'>
       <div className=' text-3xl font-semibold text-white font-mono'>
         T-BackLog
       </div>
-      <div className=' text-slate-200 mr-3 ml-auto'>games</div>
-      <div className=' px-2 py-1 bg-pink-400 text-white mr-3 ml-3'>
-        log a game
+      <div className=' text-slate-200 mr-3 ml-auto text-lg'>Games</div>
+      <SearchComponent />
+      <div className=' px-2 py-1 bg-pink-600 text-white mr-3 ml-3 font-semibold rounded-md'>
+        <MdOutlineExplore className=' inline' /> Explore
       </div>
     </div>
   );
