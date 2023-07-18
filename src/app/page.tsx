4bd8f38f51ea9a5deebc499b94f9b88a0ff3e622
Tan Nguyen <tannguyen1220@gmail.com>
2023-07-18 13:02:26 +0700

    read data from json file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,13 @@ import GameListSection from '@/component/gameListSection';
 import Header from '@/component/header';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { json } from 'stream/consumers';
 
 export default function Home() {
-  const [apiData, setApiData] = useState(null);
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const headers = {
-          'Content-Type': 'application/json',
-          Authorization: process.env.API_KEY,
-        };
-        const resData = await axios.get(
-          `https://api.rawg.io/api/games/factorio?key=c89a0b30250d4a53984fe0dbcf32ce22`
-        );
-        setApiData(resData.data);
-      } catch (error) {
-        alert(JSON.stringify(error));
-      }
-    };
-    getData();
-  }, []);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900 text-white'>
       <div className=' w-3/4 flex flex-col'>
         <Header />
         <GameListSection />
-        <div>{JSON.stringify(apiData)}</div>
       </div>
     </main>
   );
--- src/component/gameListSection/index.tsx
+++ src/component/gameListSection/index.tsx
@@ -1,18 +1,24 @@
+import { useEffect, useState } from 'react';
 import GameCard from './gameCard';
+import gameList from '@/gameList.json';
 
 export default function GameListSection() {
+  const [gamePlayed, setGamePlayed] = useState(0);
+
+  useEffect(() => {
+    setGamePlayed(gameList['game-list'].length);
+  }, []);
+
   return (
     <div className=' flex flex-col'>
       <div className=' flex flex-row text-slate-300 mt-2'>
         <div className=' mx-2 mb-2'>Games Played:</div>
-        <div>5</div>
+        <div>{gamePlayed}</div>
       </div>
       <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
-        <GameCard gameName='total-war-warhammer-ii' />
-        <GameCard gameName='total-war-warhammer' />
-        <GameCard gameName='rimworld' />
-        <GameCard gameName='the-elder-scrolls-v-skyrim' />
-        <GameCard gameName='factorio' />
+        {gameList['game-list'].map((e: any, i: number) => (
+          <GameCard key={i} gameName={e.name} />
+        ))}
       </div>
     </div>
   );
