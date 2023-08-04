45a2d48af977a8ef13a98d13e0aff73d622bb2da
Tan Nguyen <tannguyen1220@gmail.com>
2023-08-04 13:39:32 +0700

    link to home page

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,12 +1,13 @@
 import { MdOutlineExplore } from 'react-icons/md';
 import SearchComponent from './searchBar';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <div className='flex flex-row items-center p-4 border-b border-slate-600 bg-stone-900'>
-      <div className=' text-3xl font-semibold text-white font-mono'>
+      <Link href={'/'} className=' text-3xl font-semibold text-white font-mono'>
         T-BackLog
-      </div>
+      </Link>
       <div className=' text-slate-200 mr-3 ml-auto text-lg'>Games</div>
       <SearchComponent />
       <div className=' px-2 py-1 bg-pink-600 text-white mr-3 ml-3 font-semibold rounded-md'>
