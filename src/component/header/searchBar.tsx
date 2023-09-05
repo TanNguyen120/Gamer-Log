b5f5f322b0a7c6dfe6ff8e74a566445e4ef012f3
Tan Nguyen <tannguyen1220@gmail.com>
2023-09-05 11:12:42 +0700

    add working function

--- src/component/header/searchBar.tsx
+++ src/component/header/searchBar.tsx
@@ -1,6 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { FormEvent, useEffect, useState } from 'react';
+
 export default function SearchComponent() {
+  const router = useRouter();
+  const [searchString, setSearchString] = useState('');
+  useEffect(() => {
+    console.log(searchString);
+  }, [searchString]);
+
   return (
-    <form className='max-w-sm px-4 py-6'>
+    <form
+      className='max-w-sm px-4 py-6 on'
+      onSubmit={(e) => {
+        e.preventDefault();
+        router.push(`/search?word=${searchString}`);
+      }}
+    >
       <div className='relative'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
@@ -8,6 +25,7 @@ export default function SearchComponent() {
           fill='none'
           viewBox='0 0 24 24'
           stroke='currentColor'
+          onClick={() => router.push(`/search?word=${searchString}`)}
         >
           <path
             strokeLinecap='round'
@@ -17,8 +35,10 @@ export default function SearchComponent() {
           />
         </svg>
         <input
+          id='searchString'
           type='text'
           placeholder='Search'
+          onChange={(e) => setSearchString(e.target.value)}
           className='w-full py-1 pl-12 pr-4 text-slate-100  rounded-md outline-none bg-slate-600 '
         />
       </div>
