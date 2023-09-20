c17cede7a14a5ee4cf5a677eafb7f89eb79f9ac8
Tan Nguyen <tannguyen1220@gmail.com>
2023-09-20 14:00:05 +0700

    get data from api sever

--- src/component/gameListSection/index.tsx
+++ src/component/gameListSection/index.tsx
@@ -2,22 +2,30 @@ import { useEffect, useState } from 'react';
 import GameCard from './gameCard';
 import gameList from '@/gameList.json';
 import { GameCount } from './gameCount';
+import axios from 'axios';
+import { sql } from '@vercel/postgres';
 
-export default function GameListSection() {
+async function getGamesList() {
+  const games = await sql`SELECT * FROM myGames;`;
+  return games.rows;
+}
+
+export default async function GameListSection() {
+  const gameList = await getGamesList();
   return (
     <div className=' flex flex-col'>
       <div className=' flex flex-row text-slate-300 mt-2'>
         <div className=' mx-2 mb-2'>Games Played:</div>
         <div>
-          <GameCount />
+          <GameCount gamesNumber={gameList.length} />
         </div>
       </div>
       <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
-        {gameList['game-list'].map((e: any, i: number) => (
+        {gameList.map((e: any, i: number) => (
           <GameCard
             key={i}
             gameName={e.name}
-            myComment={e.myComment}
+            myComment={e.mycomment}
             gameDetails={undefined}
           />
         ))}
