d83d4c78c00063ec3cd43eef056fc0b082e54626
Tan Nguyen <tannguyen1220@gmail.com>
2023-09-27 07:50:32 +0700

    disable dangerous route

--- src/app/api/copyFromJson/route.ts
+++ src/app/api/copyFromJson/route.ts
@@ -1,14 +1,14 @@
-// import { sql } from '@vercel/postgres';
-// import { NextResponse } from 'next/server';
-// import gameList from '@/gameList.json';
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+import gameList from '@/gameList.json';
 
-// export async function GET(request: Request) {
-//   try {
-//     gameList['game-list'].forEach(async (element) => {
-//       await sql`INSERT INTO myGames (Name, myComment) VALUES (${element.name}, ${element.myComment});`;
-//     });
-//     return NextResponse.json({ result: 'done' }, { status: 200 });
-//   } catch (error) {
-//     return NextResponse.json({ error }, { status: 500 });
-//   }
-// }
+export async function GET(request: Request) {
+  try {
+    // gameList['game-list'].forEach(async (element) => {
+    //   await sql`INSERT INTO myGames (Name, myComment) VALUES (${element.name}, ${element.myComment});`;
+    // });
+    return NextResponse.json({ result: 'done' }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
