6b0b228a0772479663794a02252c3f9ee8491924
Taan <tannguyen1220@gmail.com>
2023-11-22 15:32:59 +0700

    switching btn

--- src/app/add-game/addSection.tsx
+++ src/app/add-game/addSection.tsx
@@ -1,8 +1,11 @@
 'use client';
 import axios from 'axios';
+import { AnimatePresence, motion } from 'framer-motion';
+import { type } from 'os';
 import { useEffect, useState } from 'react';
 import { AiOutlineCheckCircle, AiOutlineWarning } from 'react-icons/ai';
 
+// support call api handle
 async function addGameData(
   slug: string,
   score: any,
@@ -28,6 +31,16 @@ async function addGameData(
   }
 }
 
+// animation variation
+const labelAnimate = {
+  inactive: { scale: 0.5, color: '#475569' },
+  active: {
+    scale: 1,
+    color: '#cbd5e1',
+    transition: { duration: 1, type: 'spring', bounce: 0.8 },
+  },
+};
+
 export default function AddSection({
   slug,
   score,
@@ -41,6 +54,8 @@ export default function AddSection({
   const [resultMes, setResultMes] = useState('');
   const [messCss, setMessCss] = useState(' hidden');
   const [masterCode, setMasterCode] = useState('');
+  const [goal, setGoal] = useState('');
+  const [addPlayed, setAddPlayed] = useState(true);
   useEffect(() => {
     resultMes !== ''
       ? resultMes.includes('success')
@@ -48,8 +63,38 @@ export default function AddSection({
         : setMessCss(' text-red-600')
       : setMessCss(' hidden');
   }, [resultMes]);
+
+  useEffect(() => {
+    console.log('is add: ' + addPlayed);
+  }, [addPlayed]);
   return (
     <div className='flex flex-col pt-5'>
+      <div className='flex flex-row gap-3 my-2'>
+        <motion.div
+          variants={labelAnimate}
+          animate={addPlayed ? 'active' : 'inactive'}
+        >
+          Add to Game Played
+        </motion.div>
+        <motion.div
+          className={`h-7 w-12 p-1 rounded-lg bg-slate-400 hover:cursor-pointer flex flex-row ${
+            addPlayed ? ' justify-start' : ' justify-end'
+          }`}
+          onClick={(e) => setAddPlayed((prev) => !prev)}
+        >
+          <motion.div
+            layout
+            transition={{ type: 'spring', duration: 1, bounce: 0.7 }}
+            className='h-5 w-5 rounded-full bg-slate-200'
+          ></motion.div>
+        </motion.div>
+        <motion.div
+          variants={labelAnimate}
+          animate={!addPlayed ? 'active' : 'inactive'}
+        >
+          Add to Backlog
+        </motion.div>
+      </div>
       <div className={' font-semibold p-2 flex flex-row' + messCss}>
         <div>
           {resultMes.includes('success') ? (
@@ -61,11 +106,30 @@ export default function AddSection({
         {resultMes}{' '}
       </div>
       <div>Enter Comment:</div>
-      <textarea
-        onChange={(e) => setCommentText(e.target.value)}
-        className=' bg-neutral-800 border border-slate-400 text-slate-400 p-3'
-        spellCheck='true'
-      ></textarea>
+      <AnimatePresence>
+        {addPlayed ? (
+          <motion.textarea
+            initial={{ x: 0, opacity: 1 }}
+            exit={{ x: 100, opacity: 0 }}
+            transition={{ duration: 1 }}
+            onChange={(e) => setCommentText(e.target.value)}
+            className=' bg-neutral-800 border border-slate-400 text-slate-400 p-3'
+            spellCheck='true'
+            placeholder='add comment for this'
+          ></motion.textarea>
+        ) : (
+          <motion.textarea
+            initial={{ x: 0, opacity: 1 }}
+            exit={{ x: 100, opacity: 0 }}
+            transition={{ duration: 1 }}
+            onChange={(e) => setGoal(e.target.value)}
+            className=' bg-neutral-800 border border-slate-400 text-slate-400 p-3'
+            spellCheck='true'
+            placeholder=' add goal for this game'
+          ></motion.textarea>
+        )}
+      </AnimatePresence>
+
       <div className=' py-4'>
         <label className='block text-gray-300 text-base font-bold mb-2'>
           Master Code
