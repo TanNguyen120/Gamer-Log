d538ca0c816b0d282815ca9f4077b3c28cde77b5
Taan <tannguyen1220@gmail.com>
2023-11-25 07:17:59 +0700

    animation is fun

--- src/component/backlogListSection/backLogContent.tsx
+++ src/component/backlogListSection/backLogContent.tsx
@@ -1,8 +1,29 @@
 'use client';
-import { motion } from 'framer-motion';
+import { motion, useInView } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense, useEffect, useRef, useState } from 'react';
+
+const backlogAnimate = {
+  hideTile: {
+    opacity: 0,
+    x: -100,
+  },
+  showTile: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.8 },
+  },
+  hideGoal: {
+    opacity: 0,
+    x: 100,
+  },
+  showGoal: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.8 },
+  },
+};
 
 export default function BackLogCardContent({
   gameDetails,
@@ -11,8 +32,13 @@ export default function BackLogCardContent({
   gameDetails: any;
   goal: string;
 }) {
+  const ref = useRef(null);
+  const isView = useInView(ref);
   return (
-    <motion.div className='flex flex-col bg-neutral-800 hover:bg-neutral-700  rounded-lg border border-slate-600 h-full group'>
+    <div
+      ref={ref}
+      className='flex flex-col bg-neutral-800 hover:bg-neutral-700  rounded-lg border border-slate-600 h-full group'
+    >
       <Link href={`/game/${gameDetails.slug}`}>
         <Suspense fallback={<p>Loading Image...</p>}>
           <Image
@@ -30,11 +56,21 @@ export default function BackLogCardContent({
       </Link>
       {/* -------------------------------------------------------------------------------------------------------------------------- */}
       <div className=' flex flex-row align-middle py-5 pl-3'>
-        <motion.div className='ml:2 mr:auto my-auto '>Goal:</motion.div>
-        <motion.div className='rounded-lg bg-slate-400 opacity-20 ml-10  m-2 p-2 text-slate-700'>
+        <motion.div
+          variants={backlogAnimate}
+          animate={isView ? 'showTile' : 'hideTile'}
+          className='ml:2 mr:auto my-auto '
+        >
+          Goal:
+        </motion.div>
+        <motion.div
+          variants={backlogAnimate}
+          animate={isView ? 'showGoal' : 'hideGoal'}
+          className='rounded-lg bg-slate-400 opacity-20 ml-10  m-2 p-2 text-slate-700'
+        >
           {goal}
         </motion.div>
       </div>
-    </motion.div>
+    </div>
   );
 }
